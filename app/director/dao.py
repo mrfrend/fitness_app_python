e5a87382c500169be78df7f21f33d1a1a6819711
Ivan <ideshchenko@yandex.ru>
2025-12-13 23:41:06 +0300

    add director ui files

--- app/director/dao.py
+++ app/director/dao.py
@@ -1,1 +1,8 @@
 from app.core.dao import BaseDAO
+from app.database import db
+
+class DirectorDAO(BaseDAO):
+    def get_all(self):
+        with self.db.conn.cursor() as cursor:
+            cursor.execute("SELECT * FROM director")
+            return cursor.fetchall()
